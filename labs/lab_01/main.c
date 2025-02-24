16e769a7dd3b14d3b5fb17e3ac5fdb92ecd6e235
RomanMate <romanmate61@gmail.com>
2025-02-24 11:15:51 +0200

    moodle lab1

--- labs/lab_01/main.c
+++ labs/lab_01/main.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
+
 #include "array_functions.h"
 int main(void) {
-    printf("szumma: %i",sum(4,7));
-    printf("\nmini:%.2f",minimum(3,8,2));
-    int k, *array;
-    readArray(&k,&array,"input.txt");
-    printArray(k,array,"output.txt");
+    int n, *array;
+    readArray(&n,&array,"input.txt");
+    printArray(n,array,"CON");
     return 0;
 }
 
