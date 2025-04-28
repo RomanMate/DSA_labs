c17166cda8dd7fd434c58e8203bd9cdc3c921b18
RomanMate <romanmate61@gmail.com>
2025-04-28 11:39:59 +0300

    push

--- labs/lab_10/binariskeresofa/binarisfa.c
+++ labs/lab_10/binariskeresofa/binarisfa.c
@@ -3,3 +3,47 @@
 //
 
 #include "binarisfa.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Node *newNode(Barat new_data) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if(!new_node) {
+        exit(-1);
+    }
+    new_node->data = new_data;
+    new_node->next = NULL;
+    return new_node;
+}
+bool isEmpty(Node *head) {
+    return head == NULL;
+}
+void insertAtBeginning(Node** head_ref, Barat new_data) {
+    Node* new_node = newNode(new_data);
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+void insertAfter(Node* prev_node, Barat new_data) {
+    if (prev_node == NULL) {
+        return;
+    }
+    Node* new_node = newNode(new_data);
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
+}
+void insertAtEnd(Node** head_ref, Barat new_data) {
+    Node* new_node = newNode(new_data);
+    Node* last = *head_ref;
+    if (isEmpty(*head_ref)) {
+        *head_ref = new_node;
+        return;
+    }
+    while (last->next != NULL) last = last->next;
+    last->next = new_node;
+}
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%s\t|\t%d\t|\t%d\t|\t%d\t|\t%c\t|\t%d\t", node->data);
+        node = node->next;
+    }
+}
--- labs/lab_10/binariskeresofa/binarisfa.h
+++ labs/lab_10/binariskeresofa/binarisfa.h
@@ -20,6 +20,45 @@ typedef struct Node{
     Barat data;
     struct Node* next;
 }Node;
+/**
+ * Allocates memory for a new node
+ * @param new_data
+ * @return pointer to a new allocated node
+ */
+Node* newNode(Barat new_data);
+
+/**
+ * Checks if list is empty, head is NULL pointer
+ * @param head
+ * @return true/false
+ */
+bool isEmpty(Node *head);
+
+/**
+ * Insert new node at the beginning of the list
+ * @param head_ref
+ * @param new_data
+ */
+void insertAtBeginning(Node** head_ref, Barat new_data);
+
+/**
+ * Insert new node after a specific node
+ * @param prev_ref
+ * @param new_data
+ */
+void insertAfter(Node* prev_ref, Barat new_data);
+
+/**
+ * Insert new node at the end of the list
+ * @param head_ref
+ * @param new_data
+ */
+void insertAtEnd(Node** head_ref, Barat new_data);
+/**
+ * Prints the elements of the linked list
+ * @param head Pointer to the head of the list
+ */
+void printList(Node* head);
 
 
 #endif //BINARISFA_H
