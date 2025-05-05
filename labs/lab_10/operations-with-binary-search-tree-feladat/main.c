cb1fa1e7fb4959f118b352c10e7e82583d72bae1
RomanMate <romanmate61@gmail.com>
2025-05-05 11:53:31 +0300

    push

--- labs/lab_10/operations-with-binary-search-tree-feladat/main.c
+++ labs/lab_10/operations-with-binary-search-tree-feladat/main.c
@@ -4,35 +4,7 @@
 #include <stdlib.h>
 
 int main() {
-    /*int items[] = {7, 5, 12, 3, 6, 9, 15, 1, 4, 8, 10, 13, 17};
-    int n = 13;
-    Node *root = createNewNode(items[0]);
-    for (int i = 1; i < n; ++i) {
-        insert(root, items[i]);
-    }*/
-    /*srand(time(NULL));
-    Node* root = NULL;
-    for (int i = 1; i <= 10; ++i) {
-        int num = rand() % 401 + 500;
-        printf("%d ", num);
-        root = insert(root, num);
-    }
-    printf("\nInorder: ");
-    inorderTraversal(root);
-    */
-    /*deleteNode(root, 17);
-    printf("\nInorder: ");
-    inorderTraversal(root);
 
-    deleteNode(root, 15);
-    printf("\nInorder: ");
-    inorderTraversal(root);
-
-    deleteNode(root, 7);
-    printf("\nInorder: ");
-    inorderTraversal(root);
-
-    destroyBinaryTree(&root);*/
         freopen("data.txt", "r", stdin);
         Barat uj;
         Node *head=NULL;
@@ -50,7 +22,7 @@ int main() {
                  }
                  else
                  {
-                     insert(&head,uj);
+                     insert(head,uj);
                  }
              }
         inorderTraversal(head);
